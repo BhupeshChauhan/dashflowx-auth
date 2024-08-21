b529eaab15ba4aa1083bb964451f6b9fcb83ee74
BhupeshChauhan <erchauhanbhupesh@gmail.com>
2024-08-21 09:43:45 +0530

    Build Fix

--- src/components/DfxForgetPassword/index.stories.tsx
+++ src/components/DfxForgetPassword/index.stories.tsx
@@ -23,12 +23,7 @@ export const Basic: Story = {
       'https://images.unsplash.com/photo-1565301660306-29e08751cc53?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
     previewTitle: 'Preview Title',
     PreviewDescription: 'Preview Description',
-    handleForgotPassword: (data) => {
-      console.log(data);
-    },
     isLoading: false,
-    handleSignOn: () => {},
-    handleSignOnError: () => {},
     // logoUrl: '/DashflowLogo.png',
     varient: 'basic',
     showSignIn: true,
